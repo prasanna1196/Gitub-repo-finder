bf256f7e08f79064582f526723a441a423a37454
Prasanna Hegde <prasannanhegde@gmail.com>
2020-12-27 00:27:16 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
   
   render() {
-    const name = 'Prasanna';
-    const loading = false;
-    const showName = true;
 
     return (
       <div className="App">
-        <h1>My App</h1>
-        {loading ? <h3>Loading. . . .</h3> : <h1>Hello {name}</h1> }
+        <Navbar title="Github finder"  />
+        <UserItem />
       </div>
     );
   }
