d95a883b31d2c6ea06d22ada0a7bffe93d0ad41f
Prasanna Hegde <prasannanhegde@gmail.com>
2021-02-02 21:28:12 +0530

    ui created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
+import About from "./components/pages/About";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
 import "./App.css";
 import Users from "./components/users/Users";
+import User from "./components/users/User";
 import Alert from "./components/layout/Alert";
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   // async componentDidMount() {
@@ -34,6 +39,28 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Get single user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+  };
+
+  // Get users Repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -48,19 +75,56 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={this.state.loading} users={this.state.users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={(props) => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={this.state.user}
+                    repos={this.state.repos}
+                    loading={this.state.loading}
+                  />
+                )}
+              />
+            </Switch>
+            {/* <Search
+              searchUsers={this.searchUsers}
+              clearUsers={this.clearUsers}
+              showClear={this.state.users.length > 0 ? true : false}
+              setAlert={this.setAlert}
+            />
+            <Users loading={this.state.loading} users={this.state.users} /> */}
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import UserItem from "../users/UserItem";
 import "../../App.css";
@@ -9,6 +10,14 @@ const Navbar = ({ title }) => {
       <h1>
         <i className="fab fa-github" /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
